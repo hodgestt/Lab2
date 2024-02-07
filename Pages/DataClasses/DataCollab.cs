a215d0792f3c7fc3e87d92f56e5d6f7e67cb2374
nickpatt12 <patterns@dukes.jmu.edu>
2024-02-07 18:36:58 -0500

    added datacollab

---
+++ Pages/DataClasses/DataCollab.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab1Part3.Pages.DataClasses
+//Nick Patterson
+{
+    public class DataCollab
+    {
+        //? means this data field can contain a blank/null
+        [ForiegnKey("CollabID")]
+        public Collaboration CollabID { get; set; }
+        [ForiegnKey("DataName")]
+        public DataFile DataName { get; set; }
+    }
+}
---
+++ Pages/DataCollabs/Index.cshtml.cs
@@ -0,0 +1,37 @@
+// Nick Patterson "import statements"
+using Lab1Part3.Pages.DataClasses;
+using Lab1Part3.Pages.DB;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace Lab1Part3.Pages.DataCollabs
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<DataCollab> DataCollabTable { get; set; }
+
+        public IndexModel()
+        {
+            DataCollabTable = new List<DataCollab>();
+        }
+
+        public void OnGet()
+        {
+            SqlDataReader DataCollabReader = DBClass.DataCollabsReader();
+            while (DataCollabReader.Read())
+            {
+                DataCollabTable.Add(new DataCollab
+                {
+
+                    CollabID = Int32.Parse(DataCollabReader["CollabID"].ToString()),
+                    DataName = DataCollabReader["DataName"].ToString(),
+                }
+            );
+            }
+        
+            // Close your connection in DBClass
+            DBClass.LABTHREEDBConnection.Close();
+        }
+    }
+}
