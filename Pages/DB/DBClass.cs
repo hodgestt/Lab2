3d22f65317fa3a2fe523058c5649c8e8a97503f2
shamlojx <shamlojx@dukes.jmu.edu>
2024-02-07 15:18:23 -0500

    Here are the changes we made 11-3pm on Wed

---
+++ Pages/DB/DBClass.cs
@@ -0,0 +1,29 @@
+﻿using Lab1Part3.Pages.DataClasses;
+using System.Data.SqlClient;
+
+namespace Lab1Part3.Pages.DB
+{
+    public class DBClass
+    {
+        // The Connection Object at Data Field Level
+        public static SqlConnection LABTHREEDBConnection = new SqlConnection();
+
+        // The Connection String finds and connects to DB
+        private static readonly String? LABTHREEDBConnString = "Server=Localhost;Database=LABTHREE;Trusted_Connection=True";
+
+        //Basic Product Reader
+        public static SqlDataReader EmployeeReader()
+        {
+            SqlCommand cmdEmployeeRead = new SqlCommand();
+            cmdEmployeeRead.Connection = LABTHREEDBConnection;
+            cmdEmployeeRead.Connection.ConnectionString = LABTHREEDBConnString;
+            cmdEmployeeRead.CommandText = "SELECT * FROM Employee";
+            cmdEmployeeRead.Connection.Open(); // Open connection here, close in Model!
+
+            SqlDataReader tempReader = cmdEmployeeRead.ExecuteReader();
+
+            return tempReader;
+        }
+    }
+
+}
---
+++ Pages/DataClasses/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab1Part3.Pages.DataClasses
+{
+    public class Employee
+    {
+        //? means this data field can contain a blank/null
+        public int EmployeeID { get; set; }
+        public String? FirstName { get; set; }
+        public String? LastName { get; set; }
+        public String? Email { get; set; }
+        public String? Phone { get; set; }
+        public String? Street { get; set; }
+        public String? City { get; set; }
+        public String? State { get; set; }
+        public String? Zip { get; set;}
+        public String? UserName { get; set; }
+        public String? Password { get; set; }
+       
+    }
+}
