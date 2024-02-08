a8ff86035ebdf272ff11a1e24332cddde2539847
hodgestt <159279833+hodgestt@users.noreply.github.com>
2024-02-08 12:11:26 -0500

    Update KnowledgeItem.cs

--- Pages/DataClasses/KnowledgeItem.cs
+++ Pages/DataClasses/KnowledgeItem.cs
@@ -6,15 +6,13 @@ namespace Lab1Part3.Pages.DataClasses
 {
     public class KnowledgeItem
     {
-        public int KnowledgeItemId { get; set; }    
+        public int KnowledgeId { get; set; }    
         public String? Name { get; set;}
         public String? Subject { get; set;}
         public String? Category { get; set;}
         public String? Information { get; set;}
-        public DateTime? KnowledgeDateTime { get; set;}
+        public DateTime? KnowledgeDateTime { get; set;} 
 
-        [ForeignKey("EmployeeID")]
-        public Employee EmployeeID { get; set; }
     }
-
 }
+
