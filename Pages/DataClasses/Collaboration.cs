fd4cd8457462c459a9435c6b93a38904ad0ede05
shamlojx <shamlojx@dukes.jmu.edu>
2024-02-08 16:47:33 -0500

    made the collaboration entity

---
+++ Pages/DataClasses/Collaboration.cs
@@ -0,0 +1,19 @@
+﻿//Jessica Shamloo
+
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab1Part3.Pages.DataClasses
+{
+    public class Collaboration
+    {
+
+        //this class is just a "space" for the chat and collaboration to happen? 
+
+        //add [Required] or [BindProperty]? 
+        //I dont think soo?? nothings coming from an intake form.
+        public String NotesAndInformation { get; set; }
+        
+    }
+}
+
