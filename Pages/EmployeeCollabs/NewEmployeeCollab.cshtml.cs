b206a873e9d764e957383abfe2676a21f8e3fd8e
labpatron <labpatron@lab000001>
2024-02-09 14:46:54 -0500

    employeecollab

---
+++ Pages/EmployeeCollabs/NewEmployeeCollab.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1Part3.Pages.EmployeeCollabs
+{
+    public class NewEmployeeCollabModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
