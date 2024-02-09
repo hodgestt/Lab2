138707657d0b3b5553a760f419db28e35839055f
labpatron <labpatron@lab000001>
2024-02-09 10:59:24 -0500

    changed collaborations

---
+++ Pages/Collaborations/CollaborationsPage.cshtml.cs
@@ -0,0 +1,38 @@
+using Lab1Part3.Pages.DataClasses;
+using Lab1Part3.Pages.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace Lab1Part3.Pages.Collaborations
+{
+    public class CollaborationsPageModel : PageModel
+    {
+        public List<Collaboration> CollaborationTable { get; set; }
+
+        public CollaborationsPageModel()
+          {
+            CollaborationTable = new List<Collaboration>();
+          }
+
+        public void OnGet()
+        {
+            SqlDataReader TableReader = DBClass.TableReader();
+            while (TableReader.Read())
+            {
+                CollaborationTable.Add(new Collaboration
+                {
+                    CollabID = int.Parse(TableReader["CollabID"].ToString()),
+                    NotesAndInformation = TableReader["NotesAndInformation"].ToString(),
+                }
+                );
+            }
+
+            //add any onPost() or IActionHandler method if needed
+
+
+            DBClass.Lab1DBConnection.Close();
+        }
+    }
+}
+
