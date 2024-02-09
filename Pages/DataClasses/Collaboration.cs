2c08e76dd3404718ecdbbf33dfe37fa60ffafc96
shamlojx <shamlojx@dukes.jmu.edu>
2024-02-09 15:21:46 -0500

    push

--- Pages/DataClasses/Collaboration.cs
+++ Pages/DataClasses/Collaboration.cs
@@ -9,10 +9,6 @@ namespace Lab1Part3.Pages.DataClasses
     public class Collaboration
     {
 
-        //this class is just a "space" for the chat and collaboration to happen? 
-
-        //add [Required] or [BindProperty]? 
-        //I dont think soo?? nothings coming from an intake form.
         public int CollabID { get; set; }
 
         public String? NotesAndInformation { get; set; }
--- Pages/EmployeeCollabs/NewEmployeeCollab.cshtml.cs
+++ Pages/EmployeeCollabs/NewEmployeeCollab.cshtml.cs
@@ -1,12 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab1Part3.Pages.DB;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Lab1Part3.Pages.EmployeeCollabs
 {
     public class NewEmployeeCollabModel : PageModel
     {
+        [BindProperty]
+        public int EmployeeID { get; set; }
+
+        [BindProperty]
+        public int CollabID { get; set; }
+
+        public List<SelectListItem> Employees { get; set; }
+
+        public List<SelectListItem> CollabTeams { get; set; }
+
         public void OnGet()
         {
+            // Populate the Employee SELECT control
+            SqlDataReader EmployeeCollabReader = DBClass.GeneralReaderQuery("SELECT * FROM Employee");
+
+            Employees = new List<SelectListItem>();
+
+            while (EmployeeCollabReader.Read())
+            {
+                Employees.Add(
+                    new SelectListItem(
+                        EmployeeCollabReader["UserName"].ToString(),
+                        EmployeeCollabReader["EmployeeID"].ToString()));
+            }
+
+            DBClass.Lab1DBConnection.Close();
+
+            // Populate the Collaboration SELECT control
+            SqlDataReader CollabReader = DBClass.GeneralReaderQuery("SELECT * FROM Collaboration");
+
+            CollabTeams = new List<SelectListItem>();
+
+            while (CollabReader.Read())
+            {
+                CollabTeams.Add(
+                    new SelectListItem(
+                        CollabReader["TeamName"].ToString(),
+                        CollabReader["CollabID"].ToString()));
+            }
+
+            DBClass.Lab1DBConnection.Close();
+
+        }
+
+        public IActionResult OnPost()
+        {
+            string insertQuery = "INSERT INTO EmployeeCollab (CollabID, EmployeeID) VALUES (";
+            insertQuery += CollabID + "," + EmployeeID + ")";
+
+            DBClass.GeneralInsertQuery(insertQuery);
+
+            DBClass.Lab1DBConnection.Close();
+
+            return RedirectToPage("Index");
         }
     }
 }
+
