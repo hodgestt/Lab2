410a8efc3ec1b2cf3c77383b33ad4bd8557170d7
labpatron <labpatron@lab000001>
2024-02-09 17:39:08 -0500

    new

---
+++ Pages/KnowledgeCollabs/NewKnowledgeCollab.cshtml.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab1Part3.Pages.DataClasses;
+using Lab1Part3.Pages.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Lab1Part3.Pages.KnowledgeCollabs
+{
+    public class NewKnowledgeCollabModel : PageModel
+    {
+        [BindProperty]
+        public int KnowledgeId { get; set; }
+
+        [BindProperty]
+        public int CollabID { get; set; }
+
+        public List<SelectListItem> Knowledges { get; set; }
+
+        public List<SelectListItem> CollabTeams { get; set; }
+
+        public void OnGet()
+        {
+            // Populate the Knowledge Item SELECT control
+            SqlDataReader KnowledgeReader = DBClass.GeneralReaderQuery("SELECT * FROM KnowledgeItem");
+
+            Knowledges = new List<SelectListItem>();
+
+            while (KnowledgeReader.Read())
+            {
+                Knowledges.Add(
+                    new SelectListItem(
+                        KnowledgeReader["Name"].ToString(),
+                        KnowledgeReader["KnowledgeId"].ToString()));
+            }
+
+            DBClass.Lab1DBConnection.Close();
+
+            // Populate the Collaboration SELECT control
+            SqlDataReader CollabReader = DBClass.GeneralReaderQuery("SELECT * FROM Collaboration");
+
+            CollabTeams = new List<SelectListItem>();
+
+            while (CollabReader.Read())
+            {
+                CollabTeams.Add(
+                    new SelectListItem(
+                        CollabReader["TeamName"].ToString(),
+                        CollabReader["CollabID"].ToString()));
+            }
+
+            DBClass.Lab1DBConnection.Close();
+
+        }
+
+        public IActionResult OnPost()
+        {
+            string insertQuery = "INSERT INTO KnowledgeCollab (CollabID, KnowledgeId) VALUES (";
+            insertQuery += CollabID + "," + KnowledgeId + ")";
+
+            DBClass.GeneralInsertQuery(insertQuery);
+
+            DBClass.Lab1DBConnection.Close();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
+    
+
