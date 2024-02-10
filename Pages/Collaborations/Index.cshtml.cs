1621aa2d068b8fb5941d2872f2696c402f8a7e6c
labpatron <labpatron@lab000001>
2024-02-10 17:11:31 -0500

    collab

--- Pages/Collaborations/Index.cshtml.cs
+++ Pages/Collaborations/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Lab1Part3.Pages.DataClasses;
 using Lab1Part3.Pages.DB;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data.SqlClient;
 
 namespace Lab1Part3.Pages.Collaborations
@@ -9,6 +10,12 @@ namespace Lab1Part3.Pages.Collaborations
     public class IndexModel : PageModel
     {
         public List<Collaboration> CollaborationTable { get; set; }
+
+        public List<SelectListItem> Knowledges { get; set; }
+
+        public List<SelectListItem> EmployeeList { get; set; }
+
+        public int EmployeeID { get; set; }
 
         public IndexModel()
         {
@@ -29,10 +36,42 @@ namespace Lab1Part3.Pages.Collaborations
                 );
             }
 
-            //add any onPost() or IActionHandler method if needed
+            DBClass.Lab1DBConnection.Close();
 
+          
+            // Populate the Knowledge Item SELECT control
+            SqlDataReader EmployeeReader = DBClass.GeneralReaderQuery("SELECT EmployeeID, CONCAT(FirstName, ' ', LastName) AS Name FROM Employee");
+
+            EmployeeList = new List<SelectListItem>();
+
+            while (EmployeeReader.Read())
+            {
+                EmployeeList.Add(
+                    new SelectListItem(
+                        EmployeeReader["Name"].ToString(),
+                        EmployeeReader["EmployeeID"].ToString()));
+            }
 
             DBClass.Lab1DBConnection.Close();
+
+
+
+        }
+        public IActionResult OnPost()
+        {
+            SqlDataReader KnowledgesReader = DBClass.GeneralReaderQuery("SELECT * FROM KnowledgeItem");
+            
+            while (KnowledgesReader.Read())
+            {
+                Knowledges.Add(
+                    new SelectListItem(
+                        KnowledgesReader["Name"].ToString(),
+                        KnowledgesReader["KnowledgeId"].ToString()));
+            }
+
+            DBClass.Lab1DBConnection.Close();
+
+            return RedirectToPage("Index");
         }
     }
 }
