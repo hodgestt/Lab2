4f94dcc33e22f4e6ef2e067ddb67b45fc1d8a40f
shamlojx <shamlojx@dukes.jmu.edu>
2024-02-14 13:41:39 -0500

    push and pull

---
+++ Pages/Login/ParameterizedLogin.cshtml.cs
@@ -0,0 +1,39 @@
+//Jessica Shamloo
+
+using Microsoft.AspNetCore.Mvc;
+using Lab2.Pages.DB;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab2.Pages.Login
+{
+    public class ParameterizedLoginModel : PageModel
+    {
+        [BindProperty]
+        public string UserName { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+
+        public IActionResult OnPost()
+        {
+            if (DBClass.SecureLogin(UserName, Password) > 0)
+            {
+                HttpContext.Session.SetString("UserName", UserName);
+                ViewData["LoginMessage"] = "Login Successful!";
+            }
+            else
+            {
+                ViewData["LoginMessage"] = "Username and/or Password Incorrect";
+            }
+
+            DBClass.Lab2DBConnection.Close();
+
+            return Page();
+
+        }
+    }
+}
