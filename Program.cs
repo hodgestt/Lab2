b3f61b52a6560db07f998140c64a70f6fb9cf1f5
shamlojx <shamlojx@dukes.jmu.edu>
2024-02-16 14:21:26 -0500

    p

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Lab2.Pages.Login;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 builder.Services.AddSession(); //enabling session state
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options
+//    { OptionsBuilderConfigurationExtensions.LoginPath = "/DBLogin"; });
+
 
 var app = builder.Build();
 
@@ -18,6 +23,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+//app.UseAuthentication();
 
 app.UseSession(); //middle-ware pipeline change - enabling session state
 
