99f27a56bfe927a3efbe579c7ce99c889db5d8df
shamlojx <shamlojx@dukes.jmu.edu>
2024-02-16 15:26:37 -0500

    push

--- Pages/Plan/Index.cshtml.cs
+++ Pages/Plan/Index.cshtml.cs
@@ -2,6 +2,7 @@
 // "import statements"
 using Lab2.Pages.DataClasses;
 using Lab2.Pages.DB;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data.SqlClient;
 
@@ -17,24 +18,38 @@ namespace Lab2.Pages.Plan
             PlansTable = new List<Plans>();
         }
 
-        public void OnGet()
+
+        public IActionResult OnGet()
         {
-            SqlDataReader TableReader = DBClass.PlansReader();
-            while (TableReader.Read())
+            if (HttpContext.Session.GetString("UserName") != null) //by now, the UserName parameter and its value has already been validated
             {
-                PlansTable.Add(new Plans
+
+                SqlDataReader TableReader = DBClass.PlansReader();
+                while (TableReader.Read())
                 {
-                    PlanID = Int32.Parse(TableReader["PlanID"].ToString()),
-                    PlanName = TableReader["PlanName"].ToString(),
-                    PlanConcept = TableReader["PlanConcept"].ToString(),
-                    DateCreated = ((DateTime)TableReader["DateCreated"])
+                    PlansTable.Add(new Plans
+                    {
+                        PlanID = Int32.Parse(TableReader["PlanID"].ToString()),
+                        PlanName = TableReader["PlanName"].ToString(),
+                        PlanConcept = TableReader["PlanConcept"].ToString(),
+                        DateCreated = ((DateTime)TableReader["DateCreated"])
+                    }
+
+                );
                 }
 
-            );
-            }
+                // Close your connection in DBClass
+                DBClass.Lab2DBConnection.Close();
 
-            // Close your connection in DBClass
-            DBClass.Lab2DBConnection.Close();
+                return Page();
+            }
+            else
+            {
+                //creates a String with key of "LoginError" and a vlue of "You must login to access that page"
+                HttpContext.Session.SetString("LoginError", "You must login to access that page!");
+
+                return RedirectToPage("/Login/DBLogin");
+            }
         }
     }
 }
